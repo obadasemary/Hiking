ad8f95e581c1d22074182792493d39a386b34792
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 22:16:54 +0300

    Update FancyTimerViewModel.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Hiking/ViewModel/FancyTimerViewModel.swift
+++ Hiking/ViewModel/FancyTimerViewModel.swift
@@ -9,6 +9,15 @@ import Foundation
 import SwiftUI
 import Combine
 
-//class FancyTimer: <#super class#> {
-//    <#code#>
-//}
+class FancyTimer: ObservableObject {
+    
+    @Published var value: Int = 0
+    
+    init() {
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            
+            self.value += 1
+        }
+    }
+}
