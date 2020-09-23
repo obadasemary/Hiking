ea00638a1e66f76de6a8ba020b080d157ecb6f2d
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 22:06:52 +0300

    Create FancyTimerView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Hiking/FancyTimerView.swift
@@ -0,0 +1,20 @@
+//
+//  FancyTimerView.swift
+//  Hiking
+//
+//  Created by Abdelrahman Mohamed on 23.09.2020.
+//
+
+import SwiftUI
+
+struct FancyTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FancyTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        FancyTimerView()
+    }
+}
