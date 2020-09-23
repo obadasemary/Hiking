6393a3c0361018f768b111e6ad7aa2eee341ca32
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 21:57:16 +0300

    Create Episode.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Hiking/Models/Episode.swift
@@ -0,0 +1,13 @@
+//
+//  Episode.swift
+//  Hiking
+//
+//  Created by Abdelrahman Mohamed on 23.09.2020.
+//
+
+import Foundation
+
+struct Episode {
+    let name: String
+    let track: String
+}
