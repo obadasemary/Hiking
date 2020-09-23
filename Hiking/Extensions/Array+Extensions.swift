a9cec40240425f924bf4df796c963de48cdbd3a4
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 06:24:38 +0300

    Update Array+Extensions.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Hiking/Extensions/Array+Extensions.swift
+++ Hiking/Extensions/Array+Extensions.swift
@@ -9,6 +9,12 @@ import Foundation
 
 extension Array {
     
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+    
     func chunks(size: Int) -> [ArraySlice<Element>] {
         
         var chunks: [ArraySlice<Element>] = [ArraySlice<Element>]()
