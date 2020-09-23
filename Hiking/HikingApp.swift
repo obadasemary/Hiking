c536011d423942642f3d78016c0442d87e68d646
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 03:15:40 +0300

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Abdelrahman Mohamed on 23.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
