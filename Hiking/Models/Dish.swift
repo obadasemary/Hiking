c54f7a4c8a3465330a021025e4386d00d4f39738
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 14:11:26 +0300

    Update Dish.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Hiking/Models/Dish.swift
+++ Hiking/Models/Dish.swift
@@ -9,7 +9,7 @@ struct Dish: Identifiable {
     let name: String
     let price: Double
     let imageURL: String
-    
+    let isSpicy: Bool
 }
 
 extension Dish {
@@ -17,16 +17,13 @@ extension Dish {
     static func all() -> [Dish] {
         
         return [
-            Dish(name: "Filet Mignon", price: 35, imageURL: "e1"),
-            Dish(name: "Spagetti Meatballs", price: 12, imageURL: "e2"),
-            Dish(name: "Spagetti Meatballs", price: 12, imageURL: "e2"),
-            Dish(name: "Sushi", price: 15, imageURL: "a1"),
-            Dish(name: "Chocolate Cake", price: 8, imageURL: "d1"),
-            Dish(name: "Lemon Cake", price: 10, imageURL: "d2"),
-            Dish(name: "Spagetti Meatballs", price: 12, imageURL: "e2")
-            
+            Dish(name: "Filet Mignon", price: 35, imageURL: "kungpow", isSpicy: true),
+            Dish(name: "Spagetti Meatballs", price: 12, imageURL: "spicy", isSpicy: false),
+            Dish(name: "Spagetti Meatballs", price: 12, imageURL: "sweet", isSpicy: true),
+            Dish(name: "Sushi", price: 15, imageURL: "b1", isSpicy: true),
+            Dish(name: "Chocolate Cake", price: 8, imageURL: "d1", isSpicy: false),
+            Dish(name: "Lemon Cake", price: 10, imageURL: "d2", isSpicy: false),
+            Dish(name: "Spagetti Meatballs", price: 12, imageURL: "e2", isSpicy: true)
         ]
-        
     }
-    
 }
