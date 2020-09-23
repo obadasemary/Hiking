2da7827421b1b4fa8fa442c2fc563e3d3fa74e94
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 06:03:12 +0300

    Create MainView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Hiking/MainView.swift
@@ -0,0 +1,32 @@
+//
+//  MainView.swift
+//  Hiking
+//
+//  Created by Abdelrahman Mohamed on 23.09.2020.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "square.grid.2x2")
+                    Text("Hikings")
+                }
+            
+            AnimalsView()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Animals")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
